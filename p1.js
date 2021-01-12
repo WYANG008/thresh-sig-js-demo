75b6c6478e0d916b840e0b077954a72ddbb3b00c
WYANG008 <weitao.yang@finbook.co>
2021-01-13 01:07:08 +0800

    first commit

---
+++ p1.js
@@ -0,0 +1,3 @@
+const { Party1 } = require('@kzen-networks/thresh-sig');
+const p1 = new Party1();
+p1.launchServer();
---
+++ p2.js
@@ -0,0 +1,14 @@
+const { EcdsaParty2 } = require('@kzen-networks/thresh-sig');
+const crypto = require('crypto');
+
+const P1_ENDPOINT = 'http://localhost:8000';
+
+(async () => {
+    const party2 = new EcdsaParty2(P1_ENDPOINT);
+    const party2MasterKeyShare = await party2.generateMasterKey();
+    const party2ChildShare = party2.getChildShare(party2MasterKeyShare, 0, 0);
+    const msgHash = crypto.createHash('sha256').update('some message').digest();
+    const signature = await party2.sign(msgHash, party2ChildShare, 0, 0);
+    console.log(JSON.stringify(signature));
+    // {"r": <32-bytes-hex>,"s": <32-bytes-hex>,"recid": <0 or 1>}
+})();
